fd0c798514ef8b23b2bceea6ac80905cb044941f
DaHermann <ndanielhermann@gmail.com>
2025-10-12 21:12:55 +0100

    Develop Back-end Integration for Registration

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -24,9 +24,11 @@ app.use(express.json());
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 const giftRoutes = require('./routes/giftRoutes');
 
-
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
+
+// Auth API Task 1: import the authRoutes and store in a constant called authRoutes
+const authRoutes = require('./routes/authRoutes');
 
 
 const pinoHttp = require('pino-http');
@@ -40,6 +42,9 @@ app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
+
+//  add the authRoutes to the server by using the app.use() method.
+app.use('/api/auth', authRoutes);
 
 
 // Global Error Handler
